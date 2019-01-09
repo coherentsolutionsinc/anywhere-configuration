b608d674fcc24c56856d75132d776c91052a9b52
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2019-01-09 16:44:41 +0300

    Updated sample parameters

--- samples/different-config-sources/run-from-json.cmd
+++ samples/different-config-sources/run-from-json.cmd
@@ -1,6 +1,8 @@
+SET ANYWHERE_ADAPTER_GLOBAL_PROBING_PATH=%~dp0/Code/_adapters/Json
 SET ANYWHERE_ADAPTER_0_TYPE_NAME=CoherentSolutions.Extensions.Configuration.AnyWhere.Json.AnyWhereJsonConfigurationSourceAdapter
 SET ANYWHERE_ADAPTER_0_ASSEMBLY_NAME=CoherentSolutions.Extensions.Configuration.AnyWhere.Json
-SET ANYWHERE_ADAPTER_0_SEARCH_PATHS=Code/_adapters/Json
-SET ANYWHERE_ADAPTER_0_PATH=Code/_configs/config.json
+SET ANYWHERE_ADAPTER_0_PATH=%~dp0/Code/_configs/config.json
 
 dotnet run --project Code/Code.csproj
+
+PAUSE
--- samples/different-config-sources/run-from-key-per-file.cmd
+++ samples/different-config-sources/run-from-key-per-file.cmd
@@ -1,6 +1,8 @@
+SET ANYWHERE_ADAPTER_GLOBAL_PROBING_PATH=%~dp0/Code/_adapters/KeyPerFile
 SET ANYWHERE_ADAPTER_0_TYPE_NAME=CoherentSolutions.Extensions.Configuration.AnyWhere.KeyPerFile.AnyWhereKeyPerFileConfigurationSourceAdapter
 SET ANYWHERE_ADAPTER_0_ASSEMBLY_NAME=CoherentSolutions.Extensions.Configuration.AnyWhere.KeyPerFile
-SET ANYWHERE_ADAPTER_0_SEARCH_PATHS=Code/_adapters/KeyPerFile
-SET ANYWHERE_ADAPTER_0_DIRECTORY_PATH=Code/_configs/config
+SET ANYWHERE_ADAPTER_0_DIRECTORY_PATH=%~dp0/Code/_configs/config
 
 dotnet run --project Code/Code.csproj
+
+PAUSE

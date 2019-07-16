13cdf972152c449de81fd6b9188bb1ef7b5f7d60
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2019-07-16 16:26:55 +0300

    Implemented simple newline separated key=value parser.

---
+++ src/CoherentSolutions.Extensions.Configuration.AnyWhere.Tests/src/AnyWhereConfigurationDataKeyValueEnumeratorTests.cs
@@ -0,0 +1,481 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using CoherentSolutions.Extensions.Configuration.AnyWhere.Tests.Tools;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoherentSolutions.Extensions.Configuration.AnyWhere.Tests
+{
+    public class AnyWhereConfigurationDataKeyValueEnumeratorTests
+    {
+        public class Case : IXunitSerializable
+        {
+            public class KeyValue : IXunitSerializable
+            {
+                public string Key { get; set; }
+
+                public string Value { get; set; }
+
+                public void Serialize(
+                    IXunitSerializationInfo info)
+                {
+                    info.AddValue(nameof(this.Key), this.Key);
+                    info.AddValue(nameof(this.Value), this.Value);
+                }
+
+                public void Deserialize(
+                    IXunitSerializationInfo info)
+                {
+                    this.Key = info.GetValue<string>(nameof(this.Key));
+                    this.Value = info.GetValue<string>(nameof(this.Value));
+                }
+            }
+
+            public string DataString { get; set; }
+
+            public KeyValue[] ExpectedResults { get; set; }
+
+            public Case()
+            {
+                this.ExpectedResults = Array.Empty<KeyValue>();
+            }
+
+            public override string ToString()
+            {
+                return this.DataString.Replace(Environment.NewLine, "{nl}");
+            }
+
+            public void Serialize(
+                IXunitSerializationInfo info)
+            {
+                info.AddValue(nameof(this.DataString), this.DataString);
+                info.AddValue(nameof(this.ExpectedResults), this.ExpectedResults);
+            }
+
+            public void Deserialize(
+                IXunitSerializationInfo info)
+            {
+                this.DataString = info.GetValue<string>(nameof(this.DataString));
+                this.ExpectedResults = info.GetValue<KeyValue[]>(nameof(this.ExpectedResults));
+            }
+        }
+
+        public static IEnumerable<object[]> GetData()
+        {
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key=value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "  key=value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key  =value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "  key  =value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key=  value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key=value  ",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key=  value  ",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "  key=  value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "  key=value  ",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "  key=  value  ",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key  =  value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key  =value  ",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key  =  value  ",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = "key  =  value  ",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"{Environment.NewLine}key=value",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"key=value{Environment.NewLine}",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "key",
+                            Value = "value"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"name=vname{Environment.NewLine}fam=vfam",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "name",
+                            Value = "vname"
+                        },
+                        new Case.KeyValue()
+                        {
+                            Key = "fam",
+                            Value = "vfam"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"name=vname  {Environment.NewLine}fam=vfam",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "name",
+                            Value = "vname"
+                        },
+                        new Case.KeyValue()
+                        {
+                            Key = "fam",
+                            Value = "vfam"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"name=vname{Environment.NewLine}  fam=vfam",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "name",
+                            Value = "vname"
+                        },
+                        new Case.KeyValue()
+                        {
+                            Key = "fam",
+                            Value = "vfam"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"name=vname  {Environment.NewLine}  fam=vfam",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "name",
+                            Value = "vname"
+                        },
+                        new Case.KeyValue()
+                        {
+                            Key = "fam",
+                            Value = "vfam"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"name=vname{Environment.NewLine}{Environment.NewLine}fam=vfam",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "name",
+                            Value = "vname"
+                        },
+                        new Case.KeyValue()
+                        {
+                            Key = "fam",
+                            Value = "vfam"
+                        }
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new Case()
+                {
+                    DataString = $"name=vname{Environment.NewLine}fam=vfam{Environment.NewLine}raw=vraw",
+                    ExpectedResults = new[]
+                    {
+                        new Case.KeyValue()
+                        {
+                            Key = "name",
+                            Value = "vname"
+                        },
+                        new Case.KeyValue()
+                        {
+                            Key = "fam",
+                            Value = "vfam"
+                        },
+                        new Case.KeyValue()
+                        {
+                            Key = "raw",
+                            Value = "vraw"
+                        }
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> GetBadlyFormattedData()
+        {
+            return Array.Empty<object[]>();
+        }
+
+        [Theory]
+        [MemberData(nameof(GetData))]
+        public void Should_enumerate_all_key_values_From_data_string(
+            Case @case)
+        {
+            var enumerator = new AnyWhereConfigurationDataKeyValueEnumerator(@case.DataString);
+
+            foreach (var expectedResult in @case.ExpectedResults)
+            {
+                Assert.True(enumerator.MoveNext());
+
+                Assert.Equal(expectedResult.Key, enumerator.Current.Key);
+                Assert.Equal(expectedResult.Value, enumerator.Current.Value);
+            }
+
+            Assert.False(enumerator.MoveNext());
+        }
+
+        //[Theory]
+        //[MemberData(nameof(GetBadlyFormattedData))]
+        //public void Should_throw_exception_When_data_string_is_badly_formatted(
+        //    Case @case)
+        //{
+        //    var enumerator = new AnyWhereConfigurationDataKeyValueEnumerator(@case.DataString);
+
+        //    Assert.Throws<InvalidOperationException>(() => { enumerator.MoveNext(); });
+        //}
+
+        [Fact]
+        public void Should_enumerate_nothing_When_initialized_as_default_struct()
+        {
+            Assert.False(new AnyWhereConfigurationDataKeyValueEnumerator().MoveNext());
+        }
+
+        [Fact]
+        public void Should_throw_InvalidOperationException_When_accessing_Current_on_stale_enumerator()
+        {
+            Assert.Throws<InvalidOperationException>(
+                () => { _ = new AnyWhereConfigurationDataKeyValueEnumerator().Current; });
+        }
+    }
+}
---
+++ src/CoherentSolutions.Extensions.Configuration.AnyWhere/src/AnyWhereConfigurationDataEnumeratorKeyValue.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace CoherentSolutions.Extensions.Configuration.AnyWhere
+{
+    public struct AnyWhereConfigurationDataKeyValueEnumerator
+    {
+        private enum State
+        {
+            None = 0,
+
+            Open = 1,
+
+            Closed = 2
+        }
+
+        private const char KEY_VALUE_SEPARATOR_CHAR = '=';
+
+        private AnyWhereConfigurationDataKeyValue current;
+
+        private int inputLine;
+
+        private int inputIndex;
+
+        private string inputValue;
+
+        private State inputState;
+
+        public AnyWhereConfigurationDataKeyValueEnumerator(
+            string inputValue)
+        {
+            this.current = default;
+
+            this.inputLine = -1;
+            this.inputIndex = 0;
+            this.inputValue = inputValue;
+            this.inputState = State.None;
+        }
+
+        public AnyWhereConfigurationDataKeyValue Current
+        {
+            get
+            {
+                if (this.inputState == State.None)
+                {
+                    throw new InvalidOperationException("The enumeration wasn't started.");
+                }
+
+                return this.current;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            switch (this.inputState)
+            {
+                case State.None:
+                    if (string.IsNullOrWhiteSpace(this.inputValue))
+                    {
+                        this.inputState = State.Closed;
+                        return false;
+                    }
+
+                    this.inputState = State.Open;
+                    goto case State.Open;
+                case State.Open:
+                    var nl = Environment.NewLine.AsSpan();
+                    for (;;)
+                    {
+                        this.inputLine++;
+
+                        var input = this.inputValue.AsSpan(this.inputIndex);
+                        var index = input.IndexOf(nl);
+
+                        var output = index >= 0
+                            ? input.Slice(0, index)
+                            : input;
+
+                        var ch = 0;
+                        var ln = output.Length;
+
+                        output = output.TrimStart();
+                        if (ln > output.Length)
+                        {
+                            ch += ln - output.Length;
+                        }
+
+                        output = output.TrimEnd();
+
+                        index += nl.Length;
+
+                        if (index == (nl.Length - 1) || input.Length == index)
+                        {
+                            this.inputState = State.Closed;
+                        }
+                        else
+                        {
+                            this.inputIndex += index;
+                        }
+
+                        if (output.IsEmpty)
+                        {
+                            if (this.inputState == State.Closed)
+                            {
+                                return false;
+                            }
+
+                            continue;
+                        }
+
+                        index = output.IndexOf(KEY_VALUE_SEPARATOR_CHAR);
+                        if (index < 0)
+                        {
+                            throw new InvalidOperationException(
+                                $"Expected '{KEY_VALUE_SEPARATOR_CHAR}' at ({this.inputLine}, {ch})");
+                        }
+                        if (index == 0)
+                        {
+                            throw new InvalidOperationException(
+                                $"Expected 'a key' before '{KEY_VALUE_SEPARATOR_CHAR}' at ({this.inputLine}, {ch})");
+                        }
+                        if (output.Length - 1 == 0)
+                        {
+                            throw new InvalidOperationException(
+                                $"Expected 'a value' after '{KEY_VALUE_SEPARATOR_CHAR}' at ({this.inputLine}. {ch + 1})");
+                        }
+
+                        this.current = new AnyWhereConfigurationDataKeyValue(
+                            output.Slice(0, index).Trim().ToString(),
+                            output.Slice(index + 1).Trim().ToString());
+
+                        return true;
+                    }
+                case State.Closed:
+                    return false;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public void Reset()
+        {
+            this.current = default;
+
+            this.inputLine = -1;
+            this.inputIndex = 0;
+            this.inputValue = null;
+            this.inputState = State.None;
+        }
+    }
+}

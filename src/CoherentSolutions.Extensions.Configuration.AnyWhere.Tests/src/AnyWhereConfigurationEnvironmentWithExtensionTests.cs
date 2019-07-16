f7a87563080f91717f13499c88f0cca2bc4f6aa1
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2019-07-16 12:01:37 +0300

    Added new extension configuration environment.

---
+++ src/CoherentSolutions.Extensions.Configuration.AnyWhere.Tests/src/AnyWhereConfigurationEnvironmentWithExtensionTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using CoherentSolutions.Extensions.Configuration.AnyWhere.Tests.Tools;
+
+using Xunit;
+
+namespace CoherentSolutions.Extensions.Configuration.AnyWhere.Tests
+{
+    public class AnyWhereConfigurationEnvironmentWithExtensionTests
+    {
+        [Fact]
+        public void Should_get_value_of_item_from_extension_When_called_GetValue_when_source_doesnt_contain_requested_item()
+        {
+            var source = AnyWhereConfigurationEnvironmentMockFactory.CreateEnvironmentMock(
+                new[]
+                {
+                    ("name", "false")
+                });
+            var extension = AnyWhereConfigurationEnvironmentMockFactory.CreateEnvironmentMock(
+                new[]
+                {
+                    ("extension", "true")
+                });
+
+            var env = new AnyWhereConfigurationEnvironmentWithExtension(source.Object, extension.Object);
+
+            Assert.Equal("true", env.GetValue("extension", s => (s, true)));
+        }
+
+        [Fact]
+        public void Should_get_value_of_item_from_extension_When_called_GetValue_when_source_contains_requested_item()
+        {
+            var source = AnyWhereConfigurationEnvironmentMockFactory.CreateEnvironmentMock(
+                new[]
+                {
+                    ("name", "false")
+                });
+            var extension = AnyWhereConfigurationEnvironmentMockFactory.CreateEnvironmentMock(
+                new[]
+                {
+                    ("name", "true")
+                });
+
+            var env = new AnyWhereConfigurationEnvironmentWithExtension(source.Object, extension.Object);
+
+            Assert.Equal("true", env.GetValue("name", s => (s, true)));
+        }
+
+        [Fact]
+        public void Should_get_values_of_items_from_extension_and_then_source_When_called_GetValues()
+        {
+            var source = AnyWhereConfigurationEnvironmentMockFactory.CreateEnvironmentMock(
+                new[]
+                {
+                    ("name", "name false"),
+                    ("source", "source true")
+                });
+            var extension = AnyWhereConfigurationEnvironmentMockFactory.CreateEnvironmentMock(
+                new[]
+                {
+                    ("name", "name true"),
+                    ("extension", "extension true")
+                });
+
+            var env = new AnyWhereConfigurationEnvironmentWithExtension(source.Object, extension.Object);
+
+            var values = env.GetValues().ToArray();
+            Assert.Equal(3, values.Length);
+            
+            Assert.Equal("name", values[0].Key);
+            Assert.Equal("name true", values[0].Value);
+
+            Assert.Equal("extension", values[1].Key);
+            Assert.Equal("extension true", values[1].Value);
+
+            Assert.Equal("source", values[2].Key);
+            Assert.Equal("source true", values[2].Value);
+        }
+    }
+}

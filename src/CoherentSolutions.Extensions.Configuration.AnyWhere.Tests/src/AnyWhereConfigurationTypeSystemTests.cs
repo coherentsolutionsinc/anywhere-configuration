418ac282ed8e957bcb21f6ae7682986f2a780128
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2019-07-19 12:28:03 +0300

    Removed invalid verification (because all assemblies are loaded from the default context).

--- src/CoherentSolutions.Extensions.Configuration.AnyWhere.Tests/src/AnyWhereConfigurationTypeSystemTests.cs
+++ src/CoherentSolutions.Extensions.Configuration.AnyWhere.Tests/src/AnyWhereConfigurationTypeSystemTests.cs
@@ -16,33 +16,6 @@ namespace CoherentSolutions.Extensions.Configuration.AnyWhere.Tests
         public void Should_correctly_load_type_From_assembly()
         {
             var search = new Mock<IAnyWhereConfigurationFileSearch>();
-            search
-               .Setup(instance => instance.Find(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string[]>()))
-               .Returns(
-                    (
-                        string directory,
-                        string name,
-                        string[] extensions) =>
-                    {
-                        for (var index = 0; index < extensions.Length; index++)
-                        {
-                            var extension = extensions[index];
-                            var path = Path.Combine(directory, string.Concat(name, extension));
-                            if (File.Exists(path))
-                            {
-                                var results = new IAnyWhereConfigurationFile[extensions.Length];
-                                results[index] = AnyWhereConfigurationFileMockFactory.Create(
-                                    directory,
-                                    string.Concat(name, extension),
-                                    null);
-
-                                return results;
-                            }
-                        }
-
-                        return Array.Empty<IAnyWhereConfigurationFile>();
-                    })
-               .Verifiable();
 
             var assemblyDirectory = Directory.GetCurrentDirectory();
             var assemblyPath = Path.Combine(assemblyDirectory, "CoherentSolutions.Extensions.Configuration.AnyWhere.Tests.Adapters.dll");

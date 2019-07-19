0643c2e664d15441ba6047cc01e076ce5531aba1
Oleg Karasik <OlegKarasik@coherentsolutions.com>
2019-07-19 18:59:03 +0300

    Now input parameters are parsed and validated during configuration phase while secrets are loaded on provider load event. (#21)

--- src/CoherentSolutions.Extensions.Configuration.AnyWhere.AzureKeyVault/src/AnyWhereAzureKeyVaultConfigurationSourceAdapterConfigurationProvider.cs
+++ src/CoherentSolutions.Extensions.Configuration.AnyWhere.AzureKeyVault/src/AnyWhereAzureKeyVaultConfigurationSourceAdapterConfigurationProvider.cs
@@ -1,24 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using Microsoft.Azure.KeyVault;
 using Microsoft.Extensions.Configuration;
 
 namespace CoherentSolutions.Extensions.Configuration.AnyWhere.AzureKeyVault
 {
     public class AnyWhereAzureKeyVaultConfigurationSourceAdapterConfigurationProvider : ConfigurationProvider
     {
+        private readonly string vaultBaseUri;
+
+        private readonly KeyVaultClient client;
+
+        private readonly IReadOnlyList<AnyWhereAzureKeyVaultConfigurationSourceAdapterSecret> secrets;
+
         public AnyWhereAzureKeyVaultConfigurationSourceAdapterConfigurationProvider(
-            IReadOnlyDictionary<string, string> secrets)
+            in string vaultBaseUri,
+            in KeyVaultClient client,
+            in IReadOnlyList<AnyWhereAzureKeyVaultConfigurationSourceAdapterSecret> secrets)
         {
-            if (secrets == null)
+            if (string.IsNullOrWhiteSpace(vaultBaseUri))
             {
-                throw new ArgumentNullException(nameof(secrets));
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(vaultBaseUri));
             }
 
-            foreach (var kv in secrets)
+            this.vaultBaseUri = vaultBaseUri;
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+            this.secrets = secrets ?? throw new ArgumentNullException(nameof(secrets));
+        }
+
+        public override void Load()
+        {
+            base.Load();
+
+            var result = new Dictionary<string, string>(this.secrets.Count);
+            foreach (var secret in this.secrets)
             {
-                this.Data[kv.Key] = kv.Value;
+                var name = secret.Name;
+                var alias = secret.Alias;
+                var version = secret.Version;
+
+                if (string.IsNullOrEmpty(alias))
+                {
+                    alias = name;
+                }
+
+                result[alias] = this.client.GetSecretAsync(this.vaultBaseUri, name, version)
+                   .GetAwaiter()
+                   .GetResult()
+                   .Value;
             }
+
+            this.Data = result;
         }
     }
 }
